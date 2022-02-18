8d5f91a3418ff5f18547f1bd666ac47a2ab5ffb9
Suman Chakraborty <suchakra012@hotmail.com>
2022-02-18 22:06:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello Cloud Native Buildpack!\n")
+		io.WriteString(w, "Welcome to Cloud Native Buildpack Demo!\n")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
