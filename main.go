42e141eee8c06cf1e1abe1c2403b95c2983befa8
Suman Chakraborty <suchakra012@hotmail.com>
2022-03-18 20:59:56 +0530

    Update source code

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Welcome to GDC Cloud Bangalore webinar on Cloud Native Buildpacks !!\n")
+		io.WriteString(w, "Welcome to GDC Cloud Bangalore webinar on Cloud Native Buildpacks and Kpack !!\n")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
